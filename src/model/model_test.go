733bfc58d14b626e67c435b92d866ce193ba69ce
Masato Tokuse <masadinti@gmail.com>
2019-12-10 15:18:40 +0900

    Add model.DBConnect test case

---
+++ src/model/model_test.go
@@ -0,0 +1,16 @@
+package model
+
+import (
+  "testing"
+)
+
+// 疎通確認
+func Test_DBConnect_1(t *testing.T) {
+	db := DBConnect();
+    _, err := db.Query("SELECT * FROM task ORDER BY id DESC")
+    if err == nil {
+		t.Log("疎通確認のテストがパスしました")
+    } else {
+		t.Error("疎通確認のテストが通りません") 
+	}
+}
